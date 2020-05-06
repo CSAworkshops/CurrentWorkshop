9b8cfeeae06bba6287fb9b291ddab4b777f711d6
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2020-05-06 15:53:21 +0200

    Update from Current stream
    * Update from current stream from the 06.05.2020

--- game.js
+++ game.js
@@ -28,20 +28,63 @@ let config = {
     //Welche Funktion wird zum regelmäßigen Update verwendet == GameLoop
     update: update,
   },
+  scale: {
+    mode: Phaser.Scale.FIT,
+  }
 };
 
 //Neues Spiel erzeugen
 let game = new Phaser.Game(config);
-let image;
+let player;
+let cursors;
+let points;
+let highScore;
+const velocity = 108;
+
 //Lädt zu anfang des Spiels
 function preload() {
+  highScore = 0;
   this.load.image("rabbit", "rabbit.png");
 }
 
 //Beim erstellen des Spiels
 function create() {
-  image = this.add.image(300, 300, "rabbit");
+  sky = new Phaser.Display.Color(100, 149, 237); //#6495ED Cornflowerblue
+  this.cameras.main.setBackgroundColor(sky);
+
+  player = this.physics.add.sprite(300, 300, "rabbit").setScale(0.3);
+  player.setCollideWorldBounds(true);
+  cursors = this.input.keyboard.createCursorKeys();
+
+  var text2 = this.add.text(10, 10, 'HighScore:', { font: '30px Arial' });
+  //pink | gelb | blau | rot
+  text2.setTint(0xff00ff, 0xffff00, 0x0000ff, 0xff0000);
+  points = this.add.text(160, 10, '', { font: '30px Arial' });
+  //pink | gelb | blau | rot
+  points.setTint(0xff00ff, 0xffff00, 0x0000ff, 0xff0000);
 }
 
-//Widerholt aufgerufen -> Spielschleife / Spiellogik
-function update() {}
+//Wiederholt aufgerufen -> Spielschleife / Spiellogik
+function update() {
+  let left = cursors.left.isDown;
+  let right = cursors.right.isDown;
+  let up = cursors.up.isDown;
+  let down = cursors.down.isDown;
+  
+  if (left) {
+    player.setVelocityX(-velocity);
+  }
+
+  if (right) {
+    player.setVelocityX(velocity);
+  }
+
+  if (up) {
+    player.setVelocityY(-velocity);
+    highScore += 10;
+  }
+
+  if (!right && !left) player.setVelocityX(0);
+
+  points.text = highScore;
+}
